bb470166bae622756affba47ebc713e06803be56
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-04-07 22:01:48 +1000

    fix(metrics): Working Metrics

--- lib/metrics/router.ex
+++ lib/metrics/router.ex
@@ -12,7 +12,8 @@ defmodule Pkm.Metrics.Router do
 
   def handle_event([:router, :http], %{type: "hit"}, _route, _config) do
     Counter.inc()
-    Logger.debug("")
+    current = Counter.now()
+    Logger.debug("Counted > #{current}")
   end
 end
 
@@ -26,25 +27,26 @@ defmodule Pkm.Metrics.Counter do
     GenServer.start_link(__MODULE__, 0, name: __MODULE__)
   end
 
+  @impl true
   def init(state) do
     {:ok, state}
   end
 
-  def handle_call({:inc, state}) do
-    {:ok, state ++ 1}
+  @impl true
+  def handle_cast(:inc, state) do
+    {:noreply, state + 1}
   end
 
-  def handle_call({:now, state}) do
-    {:ok, state}
+  @impl true
+  def handle_call(:now, _from, state) do
+    {:reply, state, state}
   end
 
   def inc() do
-    GenServer.call(__MODULE__, :inc)
+    GenServer.cast(__MODULE__, :inc)
   end
 
   def now() do
-    {:ok, state} = GenServer.call(__MODULE__, :now)
-
-    state
+    GenServer.call(__MODULE__, :now)
   end
 end
