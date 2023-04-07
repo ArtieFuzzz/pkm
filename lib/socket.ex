c388792a83abf99521394bad003ce9efef453299
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-04-07 21:15:14 +1000

    refactor(socket): Rename `opr` to `op`

--- lib/socket.ex
+++ lib/socket.ex
@@ -6,16 +6,24 @@ defmodule Pkm.HTTP.WebSocket do
     {:ok, []}
   end
 
-  def handle_in({"ping", [opcode: :text]}, state) do
-    {:reply, :ok, {:text, "pong"}, state}
-  end
+  #def handle_in({"ping", [opcode: :text]}, state) do
+  #  {:reply, :ok, {:text, "pong"}, state}
+  #end
 
   #def handle_in({text, [opcode: :text]}, state) do
   #  {:reply, :ok, {:text, text}, state}
   #end
 
-  def handle_in({%{"message" => message, op: 0}, [opcode: :text]}, state) do
-    {:reply, :ok, {:text, message}, state}
+  def handle_in({payload, [opcode: :text]}, state) do
+    case Jason.decode(payload) do
+      {:ok, data} -> op(data["op"], data, state)
+      {:error, _} -> {:reply, :ok, {:text, "PAYLOAD_INVALID"}, state}
+    end
+  end
+
+  def op(0, data, state) do
+    payload = Jason.encode!(data)
+    {:reply, :ok, {:text, "You made it!\nOriginal Payload: #{payload}"}, state}
   end
 
   def terminate(:timeout, state) do
